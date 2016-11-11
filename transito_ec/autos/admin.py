6b365e5f9c11c6b6dcd662266ba03e7088c92413
María Tinitana <mltinitana@utpl.edu.ec>
2016-11-10 22:59:47 -0500

    Proyecto Transito

---
+++ transito_ec/autos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.register(Clasevehiculo)
+admin.site.register(Combustibles)
+admin.site.register(Provincias)
+admin.site.register(Tipovehiculo)
+admin.site.register(Vehiculos)
